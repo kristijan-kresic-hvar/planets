84624212b473a2c9f1f2a5cb275ba888f8444b4c
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2023-12-25 21:49:16 +0100

    Fix planet information text html tag elements

--- src/components/PlanetInformation.tsx
+++ src/components/PlanetInformation.tsx
@@ -45,12 +45,12 @@ const InformationBox = ({
       ref={ref}
       className="w-full flex flex-row md:flex-col pt-0 md:pt-[1rem] lg:pt-[1.25rem] px-[1.5rem] md:px-[0.94rem] lg:px-[1.44rem] items-center md:items-start justify-between border border-[rgba(255,255,255,0.2)]"
     >
-      <h3 className="text-opacity-50 mt-[0.44rem] md:mt-0 font-bold text-white uppercase font-spartan text-[0.5rem] lg:text-[0.6875rem] mb-[0.25rem] whitespace-nowrap">
+      <p className="text-opacity-50 mt-[0.44rem] md:mt-0 font-bold text-white uppercase font-spartan text-[0.5rem] lg:text-[0.6875rem] mb-[0.25rem] whitespace-nowrap">
         {title}
-      </h3>
-      <p className="text-white leading-normal tracking-[-0.04688rem] md:tracking-[-0.05625rem] lg:tracking-[-0.09375rem] font-antonio text-[1.25rem] md:text-[1.5rem] lg:text-[2.5rem] whitespace-nowrap my-[0.56rem] md:mb-[1.19rem] lg:mb-[1.69rem]">
-        {value}
       </p>
+      <span className="block text-white leading-normal tracking-[-0.04688rem] md:tracking-[-0.05625rem] lg:tracking-[-0.09375rem] font-antonio text-[1.25rem] md:text-[1.5rem] lg:text-[2.5rem] whitespace-nowrap my-[0.56rem] md:mb-[1.19rem] lg:mb-[1.69rem]">
+        {value}
+      </span>
     </div>
   );
 };
