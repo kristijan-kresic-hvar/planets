b620b8a07d39f01cc7462cff02637d559e754f94
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2023-12-25 21:40:36 +0100

    Create and use planet image component

--- src/components/PlanetContent.tsx
+++ src/components/PlanetContent.tsx
@@ -1,13 +1,12 @@
-import Spinner from '@components/Spinner.tsx';
 import getPlanetColor from '@utils/getPlanetColor.ts';
-import lowercase from '@utils/lowercase.ts';
 import getActivePlanetImageString from '@utils/getActivePlanetImageString.ts';
 import sourceIcon from '@assets/icon-source.svg';
 import PlanetOptions from '@components/PlanetOptions.tsx';
 import { planetOptions } from '@/constants';
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 import { Planet, PlanetOption } from '@/types';
 import { fadeIn } from '@/animations';
+import PlanetImage from '@components/PlanetImage.tsx';
 
 type PlanetContentProps = {
   activePlanet: Planet;
@@ -20,9 +19,6 @@ const PlanetContent = ({
   activeOption,
   setActiveOption,
 }: PlanetContentProps) => {
-  const [planetImageLoading, setPlanetImageLoading] = useState(true);
-
-  const geologyImageRef = useRef<HTMLDivElement | null>(null);
   const planetInfoTextRef = useRef<HTMLParagraphElement | null>(null);
 
   const showGeologyImage = activeOption.value === 'geology';
@@ -34,52 +30,17 @@ const PlanetContent = ({
     });
   }, [activePlanet, activeOption]);
 
-  useLayoutEffect(() => {
-    if (showGeologyImage) {
-      fadeIn({
-        element: geologyImageRef,
-      });
-    }
-  }, [showGeologyImage]);
-
-  useEffect(() => {
-    setPlanetImageLoading(true);
-  }, [activePlanet]);
-
   return (
     <div className="mt-[2.44rem] md:mt-[3.38rem] lg:mt-[7.88rem] lg:grid lg:grid-cols-[2fr,1fr] lg:gap-[9.56rem] mx-auto place-items-center">
       <div className="w-full flex items-center justify-center min-h-[16rem] md:min-h-[26.375rem] lg:min-h-auto">
-        <div className="relative select-none pointer-events-none">
-          <div className={`${planetImageLoading ? 'block' : 'hidden'}`}>
-            <Spinner fillColor={getPlanetColor(activePlanet.name)} />
-          </div>
-          <div className={`${planetImageLoading ? 'hidden' : 'block'}`}>
-            <img
-              className={`block object-contain h-auto max-w-full mx-auto lg:mx-0 planet-image ${lowercase(
-                activePlanet.name
-              )}`}
-              src={
-                activePlanet.images[
-                  getActivePlanetImageString(activeOption.value)
-                ]
-              }
-              onLoad={() => setPlanetImageLoading(false)}
-              alt="graphic representation of a planet"
-            />
-            <div
-              ref={geologyImageRef}
-              className={`${
-                showGeologyImage ? 'block' : 'hidden'
-              } absolute left-1/2 -translate-x-1/2 top-[63%] opacity-0`}
-            >
-              <img
-                className="object-contain h-auto max-w-[4.2rem] md:max-w-[6.8rem] lg:max-w-[10.1875rem]"
-                src={activePlanet.images.geology}
-                alt="planet geology"
-              />
-            </div>
-          </div>
-        </div>
+        <PlanetImage
+          planetName={activePlanet.name}
+          src={
+            activePlanet.images[getActivePlanetImageString(activeOption.value)]
+          }
+          geologyImageSrc={activePlanet.images.geology}
+          showGeologyImage={showGeologyImage}
+        />
       </div>
       <div className="w-full flex flex-col md:flex-row self-start md:gap-[4.31rem] justify-between lg:block mt-[2.31rem]">
         <div className="text-center md:text-left">
